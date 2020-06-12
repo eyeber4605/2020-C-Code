61f203d64da4f3ea18b45e20db6be1ab8606b07a
eyeber4605 <55225055+eyeber4605@users.noreply.github.com>
2020-06-12 13:57:21 +0900

    Update strcmp.c

--- ch11/strcmp.c
+++ ch11/strcmp.c
@@ -1,4 +1,3 @@
-//	file: strcmp.c
 #include <stdio.h>
 #include <string.h>
 
@@ -9,9 +8,11 @@ int main(void) {
 
 	s1 = "java";
 	s2 = "jav";
+	
 	printf("strcmp(%s, %s) = %d\n", s1, s2, strcmp(s1, s2));
 	s1 = "jav";
 	s2 = "java";
+	
 	printf("strcmp(%s, %s) = %d\n", s1, s2, strcmp(s1, s2));
 	printf("strncmp(%s, %s, %d) = %d\n", s1, s2, 3, strncmp(s1, s2, 3));
 
