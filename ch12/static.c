ca7cfad55045681fe57c8d815aabed8af57a7659
eyeber4605 <55225055+eyeber4605@users.noreply.github.com>
2020-06-12 14:14:47 +0900

    Update static.c

--- ch12/static.c
+++ ch12/static.c
@@ -1,5 +1,3 @@
-// file : static.c
-
 #include <stdio.h>
 
 void process();
@@ -12,9 +10,9 @@ int main() {
 	return 0;
 }
 void process() {
-	//정적 변수
+
 	static int sx;
-	//지역 변수
+
 	int x = 1;
 
 	printf("%d %d\n", x, sx);
