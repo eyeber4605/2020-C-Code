fa4608cd04371ec96d233960fcd0a91330490070
eyeber4605 <55225055+eyeber4605@users.noreply.github.com>
2020-06-12 13:58:42 +0900

    Update string.c

--- ch11/string.c
+++ ch11/string.c
@@ -1,19 +1,17 @@
-//	file : string.c
 #include <stdio.h>
 
 int main(void) {
 	char c[] = "C C++ JAVA";
 	printf("%s\n", c);
-	c[5]= '\0' ;	// NULL 문자에 의해 문자열 분리
+	c[5]= '\0' ;	
 	printf("%s\n%s\n", c, (c + 6));
 
-	//문자 배열의 각 원소를 하나 하나 출력하는 방법
-	
-	c[5] = ' ';	//널 문자를 빈 문자로 바꾸어 문자열 복원
+
+	c[5] = ' ';	
 	
 	char* p = c;
 
-	while (*p)	//(*p != '\0')도 가능
+	while (*p)	
 	
 		printf("%c", *p++);
 		
